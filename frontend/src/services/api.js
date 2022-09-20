3912f6f679b00bab46ce953335d0b3cf502bef96
cesarcosta <cesarrangelfonseca@gmail.com>
2022-09-20 15:10:50 -0300

    Created config for axios globally

---
+++ frontend/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 1000,
+  headers: {
+    'Accept': 'application/json',
+  }
+});
+
+export { api }
