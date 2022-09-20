817e8c17cb3b938bcbac5f186f67b1255c56fddf
cesarcosta <cesarrangelfonseca@gmail.com>
2022-09-20 15:09:35 -0300

    Created component to show messages

---
+++ frontend/src/components/AppMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert" :class="error ? 'alert-error' : 'alert-success'" v-if="message">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppMessage',
+  props: {
+    message: {
+      type: String, required: true
+    },
+    error: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
