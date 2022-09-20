5b7ce636b822d60537f69af8e2dd952d22b0ac5f
cesarcosta <cesarrangelfonseca@gmail.com>
2022-09-20 15:09:23 -0300

    Add Font-Awesome on app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,16 @@ import router from './router'
 
 import './assets/style.css';
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faTrash, faPencil)
+
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
