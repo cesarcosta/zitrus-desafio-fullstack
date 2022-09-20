7fbab035959b389e92a16e71e522db29a1027501
cesarcosta <cesarrangelfonseca@gmail.com>
2022-09-20 16:41:13 -0300

    Add vue mask for dates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import VueMask from '@devindex/vue-mask';
 import App from './App.vue'
 import router from './router'
 
@@ -11,9 +12,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
+import { faMoneyBillTrendUp } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faTrash, faPencil)
+library.add(faTrash, faPencil, faMoneyBillTrendUp)
 
-
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).use(VueMask).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
